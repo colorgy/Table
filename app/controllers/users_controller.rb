30f7c5ae6aa56096c12df81f7eaaf032f7165601
Neson <neson@dex.tw>
2015-07-14 23:20:03 +0800

    fix errors causes by variable change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
-	def index
-		@users = User.all
-	end
+  def index
+    @users = User.all
+  end
 
-	def show
-	    if current_user.blank?
-	      redirect_to ask_login_path
-	    else
-		  @user = User.find(params[:id])
-	      @courses_simulator_items = CoursesSimulatorItem.where(user_id: params[:id])
-	    end
-	end
+  def show
+    if current_user.blank?
+      redirect_to ask_login_path
+    else
+      @user = User.find(params[:id])
+      @user_courses = UserCourse.where(user_id: params[:id])
+    end
+  end
 end
