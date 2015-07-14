d741f0aa46bb6bcb421bc86f33e4bfd047ed2694
Neson <neson@dex.tw>
2015-07-14 17:57:41 +0800

    add SSO support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
+  include ColorgyDeviseSSOManager
+  include FlashMessageReporter
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  if ENV['DISABLE_SSO'] == 'true'
+    before_filter :sso_off!
+  end
 end
