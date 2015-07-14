5eca0a6615971a44b52b479e1212ec8f6ddf81c0
ChengHsuanLiu <chenghsuango@gmail.com>
2015-07-15 00:41:37 +0800

    fix error cause by variable changed

--- app/controllers/course_users_controller.rb
+++ app/controllers/course_users_controller.rb
@@ -1,7 +1,7 @@
 class CourseUsersController < ApplicationController
 
 	def show
-		@courses_simulator_item = CoursesSimulatorItem.includes(:user).where(course_code: params[:id])
+		@courses_simulator_item = UserCourse.includes(:user).where(course_code: params[:id])
 		@users = @courses_simulator_item.map(&:user)
 		render json: @users
 	end
