82651d1a973aa5f766be480a214eba12b0674bb9
ChengHsuanLiu <chenghsuango@gmail.com>
2015-07-14 21:30:00 +0800

    add user show panel

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,11 @@ class UsersController < ApplicationController
 	end
 
 	def show
-		@user = User.find(params[:id])
+	    if current_user.blank?
+	      redirect_to ask_login_path
+	    else
+		  @user = User.find(params[:id])
+	      @courses_simulator_items = CoursesSimulatorItem.where(user_id: params[:id])
+	    end
 	end
 end
