1e1bd61c957ba7af5c0370af105295dcd764a4ec
ChengHsuanLiu <chenghsuango@gmail.com>
2015-07-16 06:36:15 +0800

    讓使用者可以瀏覽自己追蹤了誰

--- app/controllers/user_followed_user_controller.rb
+++ app/controllers/user_followed_user_controller.rb
@@ -1,26 +1,34 @@
 class UserFollowedUserController < ApplicationController
   def index
     @user_followed_users = current_user.user_followed_users
-    @user = User.all
   end
 
   def create
     @user_followed_user = current_user.user_followed_users.new(user_followed_user_params)
-      respond_to do |format|
-        if @user_followed_user.save
-          format.html { redirect_to :back, notice: 'Add to wish list successfully!' }
-        else
-          format.html { redirect_to :back, notice: 'Add to wish list failed!' }
-        end
+    respond_to do |format|
+      if @user_followed_user.save
+        format.json {
+          render json:{
+            user_followed_user: @user_followed_user
+          }
+        }
+      else
+        format.json {render json: { status: "failed" }}
       end
+    end
   end
 
   def destroy
     @user_followed_user = current_user.user_followed_users.find(params[:id])
     @user_followed_user.destroy
-      respond_to do |format|
-        format.html { redirect_to :back, notice: 'Item was successfully destroyed.' }
-      end
+    respond_to do |format|
+      format.json {
+        render json:{
+          status: "Item was successfully destroyed.",
+          user_followed_user: @user_followed_user
+        }
+      }
+    end
   end
 
   private
