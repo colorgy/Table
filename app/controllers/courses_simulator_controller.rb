3d6aac9cb42b9f9144cd569c22a5470e34003f85
ChengHsuanLiu <chenghsuango@gmail.com>
2015-07-16 06:34:26 +0800

    阻擋沒有 學校代碼的使用者

--- app/controllers/courses_simulator_controller.rb
+++ app/controllers/courses_simulator_controller.rb
@@ -3,7 +3,11 @@ class CoursesSimulatorController < ApplicationController
     if current_user.blank?
       redirect_to ask_login_path
     else
-      @user_courses = current_user.user_courses
+      if current_user.organization_code.blank?
+        redirect_to ask_login_path
+      else
+        @user_courses = current_user.user_courses
+      end
     end
   end
 
@@ -12,14 +16,12 @@ class CoursesSimulatorController < ApplicationController
 
     respond_to do |format|
       if @courses_simulator_item.save
-        # format.html { redirect_to :back, notice: 'Add to wish list successfully!' }
         format.json { render json:
           {
             item: @courses_simulator_item
           }
         }
       else
-        # format.html { redirect_to :back, notice: 'Add to wish list failed!' }
         format.json {render json: { status: "failed" }}
       end
     end
@@ -29,7 +31,6 @@ class CoursesSimulatorController < ApplicationController
     @courses_simulator_item = current_user.user_courses.find(params[:id])
     @courses_simulator_item.destroy
     respond_to do |format|
-      # format.html { redirect_to :back, notice: 'Item was successfully destroyed.' }
       format.json { render json:
         {
           status: "Item was successfully destroyed.",
