156234b157f04705c577c1030d9516fc51934558
ChengHsuanLiu <chenghsuango@gmail.com>
2015-08-09 01:12:45 +0800

    讓使用者搜尋時 取消isotope 改用 params 讓 pagination 正常

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,13 @@ class UsersController < ApplicationController
       flash[:error] = "請先登入才能進行此操作"
       redirect_to landing_page_path
     else
-      @users = User.all.paginate(:page => params[:page], :per_page => 25)
+      #@users = User.all
       @user_followed_users = current_user.user_followed_users
+      @users = User.all.paginate(:page => params[:page], :per_page => 25)
+      name = "%" + params[:name] + "%" if params[:name].present?
+      organization_code = "%" + params[:organization_code] + "%" if params[:organization_code].present?
+      @users = @users.where("lower(name) LIKE ?", name.downcase).paginate(:page => params[:page], :per_page => 25) if params[:name].present? && params[:name] != '' && params[:name] != 'undefined'
+      @users = @users.where("organization_code LIKE ?", organization_code).paginate(:page => params[:page], :per_page => 25) if params[:organization_code].present? && params[:organization_code] != 'all'
     end
   end
 
@@ -41,7 +46,10 @@ class UsersController < ApplicationController
       redirect_to landing_page_path
     else
       name = "%" + params[:name] + "%"
-      @found_user = User.where("lower(name) LIKE ?", name.downcase)
+      organization_code = "%" + params[:organization_code] + "%"
+      @found_user = User.all
+      @found_user = @found_user.where("lower(name) LIKE ?", name.downcase)
+      @found_user = @found_user.where("organization_code LIKE ?", organization_code)
 
       respond_to do |format|
         if @found_user
@@ -56,4 +64,33 @@ class UsersController < ApplicationController
       end
     end
   end
+
+  def shared_callback
+    if current_user
+      current_user.shared = true
+      current_user.save!
+    end
+
+    render json: {}
+  end
+
+  def subscribed_callback
+    if current_user
+      current_user.subscribed = true
+      current_user.save!
+    end
+
+    render json: {}
+  end
+
+  def first_login_callback
+    if current_user
+      current_user.first_login = false
+      current_user.save!
+    end
+
+    render json: {}
+  end
+
+
 end
