a45083af38bb24cef27937ef26219c8088588b89
Neson <neson@dex.tw>
2015-08-11 19:46:40 +0800

    setup followers counter cache for users

--- app/models/user_followed_user.rb
+++ app/models/user_followed_user.rb
@@ -1,6 +1,7 @@
 class UserFollowedUser < ActiveRecord::Base
   belongs_to :user, class_name: :User, foreign_key: :user_id
-  belongs_to :followed_user, class_name: :User, foreign_key: :followed_user_id
+  belongs_to :followed_user, class_name: :User, foreign_key: :followed_user_id,
+                             counter_cache: :followers_count
 
   validates :user, :followed_user, presence: true
   validates :followed_user_id, uniqueness: { scope: :user_id }
