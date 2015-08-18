313cbd88645b536d51d41cc86c615afdda41a420
ChengHsuanLiu <chenghsuango@gmail.com>
2015-08-19 02:11:57 +0800

    只能看到自己學校的問課

--- app/controllers/ask_courses_controller.rb
+++ app/controllers/ask_courses_controller.rb
@@ -1,22 +1,37 @@
 class AskCoursesController < ApplicationController
   def index
-    @ask_courses = AskCourse.all.paginate(:page => params[:page], :per_page => 10)
-    course_general_code = "%" + params[:course_general_code] + "%" if params[:course_general_code].present?
-    course_name = "%" + params[:course_name] + "%" if params[:course_name].present?
-    course_lecturer = "%" + params[:course_lecturer] + "%" if params[:course_lecturer].present?
-    @ask_courses = @ask_courses.where("course_general_code LIKE ?", course_general_code).paginate(:page => params[:page], :per_page => 10) if params[:course_general_code].present? && params[:course_general_code] != '' && params[:course_general_code] != 'undefined'
-    @ask_courses = @ask_courses.where("course_name LIKE ?", course_name).paginate(:page => params[:page], :per_page => 10) if params[:course_name].present? && params[:course_name] != '' && params[:course_name] != 'undefined'
-    @ask_courses = @ask_courses.where("course_lecturer LIKE ?", course_lecturer).paginate(:page => params[:page], :per_page => 10) if params[:course_lecturer].present? && params[:course_lecturer] != '' && params[:course_lecturer] != 'undefined'
+    if current_user.blank?
+      flash[:error] = "請先登入才能進行此操作"
+      redirect_to landing_page_path
+    else
+      @ask_courses = AskCourse.where(course_organization_code: current_user.organization_code).paginate(:page => params[:page], :per_page => 10)
+      course_general_code = "%" + params[:course_general_code] + "%" if params[:course_general_code].present?
+      course_name = "%" + params[:course_name] + "%" if params[:course_name].present?
+      course_lecturer = "%" + params[:course_lecturer] + "%" if params[:course_lecturer].present?
+      @ask_courses = @ask_courses.where("course_general_code LIKE ?", course_general_code).paginate(:page => params[:page], :per_page => 10) if params[:course_general_code].present? && params[:course_general_code] != '' && params[:course_general_code] != 'undefined'
+      @ask_courses = @ask_courses.where("course_name LIKE ?", course_name).paginate(:page => params[:page], :per_page => 10) if params[:course_name].present? && params[:course_name] != '' && params[:course_name] != 'undefined'
+      @ask_courses = @ask_courses.where("course_lecturer LIKE ?", course_lecturer).paginate(:page => params[:page], :per_page => 10) if params[:course_lecturer].present? && params[:course_lecturer] != '' && params[:course_lecturer] != 'undefined'
+    end
   end
 
   def show
-    @ask_course = AskCourse.find(params[:id])
-    @user = User.find(@ask_course.user_id)
-    @course_comments = CourseComment.where(ask_course_id: @ask_course.id).paginate(:page => params[:page], :per_page => 10)
+    if current_user.blank?
+      flash[:error] = "請先登入才能進行此操作"
+      redirect_to landing_page_path
+    else
+      @ask_course = AskCourse.find(params[:id])
+      @user = User.find(@ask_course.user_id)
+      @course_comments = CourseComment.where(ask_course_id: @ask_course.id).paginate(:page => params[:page], :per_page => 10)
+    end
   end
 
   def new
-    @ask_course = AskCourse.new
+    if current_user.blank?
+      flash[:error] = "請先登入才能進行此操作"
+      redirect_to landing_page_path
+    else
+      @ask_course = AskCourse.new
+    end
   end
 
   def create
