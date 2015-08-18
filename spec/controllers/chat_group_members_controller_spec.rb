237aab737d128ea596e6be7c2a2bf4d7b25b796f
ChengHsuanLiu <chenghsuango@gmail.com>
2015-08-19 01:25:43 +0800

    change spec file name

---
+++ spec/controllers/chat_group_members_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe ChatGroupMembersController, type: :controller do
+
+end
---
+++ spec/controllers/chat_groups_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe ChatGroupsController, type: :controller do
+
+end
