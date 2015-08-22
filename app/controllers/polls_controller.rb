0eec166eace7bd649ec2964012dc4884f1153172
ChengHsuanLiu <chenghsuango@gmail.com>
2015-08-23 05:25:15 +0800

    add poll controller & model

---
+++ app/controllers/polls_controller.rb
@@ -0,0 +1,49 @@
+class PollsController < ApplicationController
+
+  def index
+
+  end
+
+  def create
+    @poll = current_user.polls.new(poll_params)
+    respond_to do |format|
+      if @poll.save
+        format.json {
+          render json:{
+            poll: @poll
+          }
+        }
+      else
+        format.json {render json: { status: "failed" }}
+      end
+    end
+  end
+
+  def get_poll_result
+    gender = params[:gender]
+    @poll = Poll.all.where(user_polled_user_gender: gender)
+    @poll_fresh = @poll.where(user_polled_user_started_year: 2015)
+    @poll_total = @poll.count
+    @poll_fresh_total = @poll_fresh.count
+
+    respond_to do |format|
+      if @poll_total && @poll_fresh_total
+        format.json {
+          render json:{
+            poll_total: @poll_total,
+            poll_fresh_total: @poll_fresh_total
+          }
+        }
+      else
+        format.json {render json: { status: "failed" }}
+      end
+    end
+
+  end
+
+  private
+
+  def poll_params
+    params.require(:poll).permit(:user_id,:user_polled_user_id,:user_polled_user_gender,:user_polled_user_started_year)
+  end
+end
--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,2 +1,3 @@
 class Poll < ActiveRecord::Base
+  belongs_to :user
 end
